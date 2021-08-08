fe1644f188dab0e6f7f0d5a403c02cc583dbf879
Joseph Vega <jcvb0206@gmail.com>
2021-08-08 13:38:53 -0500

    Create categories service to fetch categories data and update script.js to run SPA

--- src/script.js
+++ src/script.js
@@ -1,1 +1,12 @@
-import STORE from './assets/scripts/store.js';
+import STORE from './scripts/store.js';
+import ProductsService from './scripts/services/products_service.js';
+
+async function init() {
+  // const Home = new Home('.js-app');
+  const productsService = new ProductsService();
+  STORE.products = await productsService.list();
+  console.log(STORE.products);
+  // home.render();
+}
+
+init();

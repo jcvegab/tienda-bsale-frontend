72649bd85a13cd6ad1ba40c2349eff097c498404
Joseph Vega <jcvb0206@gmail.com>
2023-12-10 15:16:21 -0500

    Migrate to Static
    Add vercel config

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-import STORE from './scripts/store.js';
-import Home from './scripts/home.js';
-import CategoriesService from './scripts/services/categories_service.js';
-import ProductsService from './scripts/services/products_service.js';
+import STORE from "./scripts/store.js";
+import Home from "./scripts/home.js";
+import CategoriesService from "./scripts/services/categories_service.js";
+import ProductsService from "./scripts/services/products_service.js";
 
 async function init() {
-  const home = new Home('.js-app');
+  const home = new Home(".js-app");
   const categoriesService = new CategoriesService();
   STORE.categories = await categoriesService.list();
   const productsService = new ProductsService();
